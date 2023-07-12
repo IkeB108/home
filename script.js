8689d17a9b4aa293d6f8055d65aceeb62cdace43
IkeB108 <qwertyaeb@gmail.com>
2023-07-11 20:52:41 -0700

    Accessibility fixes and other fixes

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+console.log("Press 's' to save the webpage contents to the clipboard.")
+document.onkeypress = function(e) { 
+  if(e.key == 's'){
+    c = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n  <meta charset=\"UTF-8\" />\n  <title>Ikebot</title>\n  <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" />\n  <meta name=\"description\" content=\"\" />\n  <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />\n  <link rel=\"icon\" href=\"favicon.png\">\n</head>"
+    c += `<body style="background-color:black" onload="onLoad()">\n`
+    c += document.body.innerHTML.split("<!-- Code injected by live-server -->")[0]
+    c += "\n"
+    c += `</body>\n</html>`
+    navigator.clipboard.writeText( c )
+    alert("Document is saved to clipboard")
+  }
+}
+
 function onLoad(){
-  console.log("LOADED")
   fetch("description/projectDescriptions.json")
     .then((response) => response.json())
     .then(generateProjectCards)
@@ -15,7 +27,7 @@ function generateProjectCards(json){
   for(let p in pd){
     //For each project, create a project card.
     
-    //Make new project cards by cloning the tempalte project card,
+    //Make new project cards by cloning the template project card,
     //which we will delete at the end.
     let project = pd[p]
     let newProjectCard = template.clone()
@@ -33,21 +45,22 @@ function generateProjectCards(json){
     newProjectCard.find(".pCardDescription").text(project.shortDescription)
     newProjectCard.find(".pCardDate").text(project.date)
     newProjectCard.find(".pCardImageContainer").css("background-image", `url("thumbnails/${project.thumbnail}")`)
+    newProjectCard.find(".pCardImageContainer").attr("title", project.thumbnailAlt)
     
     //Add any icons that apply (interact, youtube, and/or game)
     let iconIndex = 7;
     if(project.icons.toLowerCase().includes("interact")){
-      let newIconDiv = "<div class=\"iconDiv interactIcon\"><image src=\"icons/interactIcon.png\" class=\"iconImage\"></image></div>"
+      let newIconDiv = "<div class=\"iconDiv interactIcon\"><img src=\"icons/interactIcon.png\" class=\"iconImage\" alt=\"interactive\"></img></div>"
       newProjectCard.find(".pCardImageContainer").find(".emptyDiv").eq(iconIndex).replaceWith(newIconDiv)
       iconIndex --
     }
     if(project.icons.toLowerCase().includes("youtube")){
-      let newIconDiv = "<div class=\"iconDiv youtubeIcon\"><image src=\"icons/youtubeIcon.png\" class=\"iconImage\"></image></div>"
+      let newIconDiv = "<div class=\"iconDiv youtubeIcon\"><img src=\"icons/youtubeIcon.png\" class=\"iconImage\" alt=\"YouTube\"></img></div>"
       newProjectCard.find(".pCardImageContainer").find(".emptyDiv").eq(iconIndex).replaceWith(newIconDiv)
       iconIndex --
     }
     if(project.icons.toLowerCase().includes("game")){
-      let newIconDiv = "<div class=\"iconDiv gameIcon\"><image src=\"icons/gameIcon.png\" class=\"iconImage\"></image></div>"
+      let newIconDiv = "<div class=\"iconDiv gameIcon\"><img src=\"icons/gameIcon.png\" class=\"iconImage\" alt=\"game\"></img></div>"
       newProjectCard.find(".pCardImageContainer").find(".emptyDiv").eq(iconIndex).replaceWith(newIconDiv)
       iconIndex --
     }
